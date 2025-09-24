446be946ec779016f61b473cebd49a5c994a133d
rakeshsah0 <rakeshsah192837465@gmail.com>
2025-09-24 19:45:22 +0530

    created container deploy script for github action

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJson = require("../package.json");
@@ -13,14 +13,6 @@ const devConfig = {
     },
   },
   plugins: [
-    new ModuleFederationPlugin({
-      name: "marketing",
-      filename: "remoteEntry.js",
-      exposes: {
-        "./MarketingApp": "./src/bootstrap",
-      },
-      shared: packageJson.dependencies,
-    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
