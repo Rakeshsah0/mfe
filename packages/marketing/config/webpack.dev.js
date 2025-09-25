f774d0c35f7a815bab25c0b816b10dc3fd51b6ef
rakeshsah0 <rakeshsah192837465@gmail.com>
2025-09-25 15:26:01 +0530

    added header

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -3,6 +3,7 @@ const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJson = require("../package.json");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const devConfig = {
   mode: "development",
@@ -13,6 +14,14 @@ const devConfig = {
     },
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      shared: packageJson.dependencies,
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
